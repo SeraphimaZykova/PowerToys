d82c9b66e7dc8370b833af9bc9aa654abade21d7
Serafima Zykova <zykovas91@gmail.com>
2020-02-04 18:41:25 +0300

    check on guid convertion success

--- src/modules/fancyzones/lib/ZoneWindow.cpp
+++ src/modules/fancyzones/lib/ZoneWindow.cpp
@@ -498,9 +498,11 @@ ZoneWindow::SaveWindowProcessToZoneIndex(HWND window) noexcept
         if (zoneIndex != -1)
         {
             OLECHAR* guidString;
-            StringFromCLSID(m_activeZoneSet->Id(), &guidString);
+            if (StringFromCLSID(m_activeZoneSet->Id(), &guidString) == S_OK)
+            {
+                JSONHelpers::FancyZonesDataInstance().SetAppLastZone(window, m_uniqueId, guidString, zoneIndex);
+            }
 
-            JSONHelpers::FancyZonesDataInstance().SetAppLastZone(window, m_uniqueId, guidString, zoneIndex);
             CoTaskMemFree(guidString);
         }
     }
